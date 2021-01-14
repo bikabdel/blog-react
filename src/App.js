07e4acb682091cebcfc84acce9b0e2508d5a5e99
bikabd <abdelmoula.bikourne@gmail.com>
2021-01-14 09:23:36 +0100

    project structure

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <div className="content">
+        <h1>BLOG</h1>
+        {/* Components */}
+      </div>
+    </div>
+  );
+}
+
+export default App;
